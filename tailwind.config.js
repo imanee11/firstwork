6095a02c6d6938529ae8407762be6593a608029b
imanee11 <imaneelouannane@gmail.com>
2025-01-15 15:06:37 +0100

    add colors in talwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#669295',   
+        secondary: '#cfe1e5', 
+        accent: '#cfe1e5',  
+      },
+    },
   },
   plugins: [],
 }
