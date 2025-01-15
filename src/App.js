0126a75a8ccac05b70db40acf991ea924cb0b99b
imanee11 <imaneelouannane@gmail.com>
2025-01-15 14:52:45 +0100

    add some folders

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Home from './pages/home';
 
 const App = () => {
   return (
     <>
+    <Home/>
       
     </>
   );
---
+++ src/pages/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from '../components/home/NavBar';
+
+const Home = () => {
+    return (
+        <>
+        <NavBar/>
+            
+        </>
+    );
+};
+
+export default Home;
