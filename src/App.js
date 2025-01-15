5647721e96d257c4c428ddc5f0246253afd6a7c7
imanee11 <imaneelouannane@gmail.com>
2025-01-15 14:57:43 +0100

    add react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Home from './pages/home';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <>
-    <Home/>
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/todo" element={<TodoPage />} /> */}
+      </Routes>
     </>
   );
 };
